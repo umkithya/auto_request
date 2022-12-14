3be51d0600c18e57c8de8b637aae56fee2b7ef05
umkithya <kithya.um@z1fsc.com>
2022-12-14 14:56:08 +0700

    My first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require("express");
+const app = express();
+const request = require('request')
+var cron = require('node-cron');
+
+
+function runRequest() {
+        request({
+        url: "https://happymovieapp.onrender.com/",
+        method: "GET",
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10
+    },function(error, response, body){
+        if(!error && response.statusCode == 200){
+            console.log('sucess!'+body);
+        }else{
+            console.log('error' + response.statusCode);
+        }
+    });
+}
+
+var num = 0
+  
+var myInterval =  setInterval(() => {
+  
+   // Increment post tracker
+   num++
+   console.log('Wait for 2 second...')
+   runRequest()
+
+}, 300000)
+
+function stop() {
+    clearInterval(myInterval);
+}
+function start() {
+    myInterval();
+}
